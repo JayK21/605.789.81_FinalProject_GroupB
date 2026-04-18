658f184f63b9d63aebc98fbaf7ca2804a1191ce1
Pablo Ruiz <pablodruiz928@gmail.com>
2026-04-18 12:49:14 -0400

    Populate user and event models

--- src/main/java/edu/jhu/eventservice/models/Event.java
+++ src/main/java/edu/jhu/eventservice/models/Event.java
@@ -1,5 +1,134 @@
 package edu.jhu.eventservice.models;
 
-public class Event {
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "event")
+public class Event {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer eventId;
+
+    @Column(name = "title", unique = true, nullable = false)
+	private String title;
+    
+    @Column(name = "description", unique = false, nullable = true)
+	private String description;
+    
+    @Column(name = "location", unique = false, nullable = false)
+	private String location;
+    
+    @Column(name = "date", unique = false, nullable = false)
+	private LocalDate date;
+    
+    @Column(name = "time", unique = false, nullable = false)
+	private LocalTime time;
+    
+    @Column(name = "maxCapacity", unique = false, nullable = true)
+	private Integer maxCapacity;
+    
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = true)
+    @JsonBackReference
+	private List<User> attendees;
+    
+    public Event() {}
+
+	public Event(Integer eventId, String title, String description, String location, LocalDate date, LocalTime time,
+			Integer maxCapacity, List<User> attendees) {
+		super();
+		this.eventId = eventId;
+		this.title = title;
+		this.description = description;
+		this.location = location;
+		this.date = date;
+		this.time = time;
+		this.maxCapacity = maxCapacity;
+		this.attendees = attendees;
+	}
+
+	public Integer getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(Integer eventId) {
+		this.eventId = eventId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	public LocalTime getTime() {
+		return time;
+	}
+
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
+
+	public Integer getMaxCapacity() {
+		return maxCapacity;
+	}
+
+	public void setMaxCapacity(Integer maxCapacity) {
+		this.maxCapacity = maxCapacity;
+	}
+
+	public List<User> getAttendees() {
+		return attendees;
+	}
+
+	public void setAttendees(List<User> attendees) {
+		this.attendees = attendees;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [eventId=" + eventId + ", title=" + title + ", description=" + description + ", location="
+				+ location + ", date=" + date + ", time=" + time + ", maxCapacity=" + maxCapacity + ", attendees="
+				+ attendees + "]";
+	}
+    
 }
--- src/main/java/edu/jhu/eventservice/models/User.java
+++ src/main/java/edu/jhu/eventservice/models/User.java
@@ -1,5 +1,89 @@
 package edu.jhu.eventservice.models;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 
+@Entity
+@Table(name = "course")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer userId;
+
+    @Column(name = "firstName", unique = true, nullable = false)
+	private String firstName;
+    
+    @Column(name = "lastName", unique = true, nullable = false)
+	private String lastName;
+    
+    @Column(name = "email", unique = true, nullable = false)
+	private String email;
+    
+    @Column(name = "password", unique = true, nullable = false)
+	private String password;
+    
+    public User() {}
+    
+    public User(Integer userId, @NotBlank(message = "First name is required") String firstName,
+			@NotBlank(message = "Last name is required") String lastName,
+			@NotBlank(message = "Email is required") String email, 
+			@NotBlank(message = "Password is required") String password) {
+    	this.firstName = firstName;
+    	this.lastName = lastName;
+    	this.email = email;
+    	this.password = password;   	
+    }
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + "]";
+	}
+    
+    
 }
